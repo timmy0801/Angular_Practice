eaeb51456886cda516f711c8ab4c136758b86b01
timmy <lintimtim123@gmail.com>
2022-05-26 01:19:42 +0800

    day-6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { stationList } from './station-list.const';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularPractice';
+  list = stationList;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// import { StationList } from './station-list.const.ts/station-list.const.ts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // StationList.Const.TsComponent
   ],
   imports: [
     BrowserModule,
